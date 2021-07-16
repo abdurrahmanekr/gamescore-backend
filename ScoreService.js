29852a0cf425e9f8d2ab01ec728e7ccda4bd8a37
abdurrahmanekr <abdurrahmaneker58@gmail.com>
2021-07-16 19:42:06 +0300

    scoreservice taşıma

---
+++ ScoreService.js
@@ -0,0 +1,98 @@
+const { promisify } = require('util');
+
+let setValue, getValue, mGet, zAdd, zIncrby, zRevrank, zCard, zRevrange, ZMscore;
+class ScoreService {
+	async redisInit(redisClient) {
+		setValue = promisify(redisClient.set).bind(redisClient);
+		getValue = promisify(redisClient.get).bind(redisClient);
+		mGet = promisify(redisClient.mget).bind(redisClient);
+		zAdd = promisify(redisClient.zadd).bind(redisClient);
+		zIncrby = promisify(redisClient.zincrby).bind(redisClient);
+		zRevrank = promisify(redisClient.zrevrank).bind(redisClient);
+		zCard = promisify(redisClient.zcard).bind(redisClient);
+		zRevrange = promisify(redisClient.zrevrange).bind(redisClient);
+		ZMscore = promisify(redisClient.zmscore).bind(redisClient);
+	}
+
+	async getRangeUsers(min, max) {
+	    if (min === max)
+	        return [];
+
+	    const userScores = await zRevrange(['user_money', min, max, 'WITHSCORES']);
+
+	    if (userScores.length < 1)
+	        return [];
+
+	    const userIds = [];
+	    const moneys = [];
+	    userScores.forEach((x, i) => {
+	        if (i % 2 === 0)
+	            userIds.push(x);
+	        else
+	            moneys.push(x);
+	    });
+
+	    const userTodayScores = await ZMscore(['user_today_money', ...userIds]);
+
+	    const users = (await mGet(userIds.map(x => `user_${x}`))).map(JSON.parse);
+	    let index = min;
+	    users.forEach((x, i) => {
+	        x.money = moneys[i];
+	        x.todayRank = userTodayScores[i];
+	        x.rank = index++;
+	    });
+
+	    return users;
+	}
+
+	async getTop100() {
+	    return await this.getRangeUsers(0, 99);
+	}
+
+	async checkUserAndCreate(user) {
+		const redisUser = await zRevrank(['user_money', user.id]);
+        if (redisUser === null) {
+            await zAdd(['user_money', 0, user.id]);
+            const rank = await zRevrank(['user_money', user.id]);
+            await zAdd(['user_today_money', rank, user.id])
+
+            await setValue(`user_${user.id}`, JSON.stringify({
+                id: user.id,
+                name: user.name,
+                country: user.country,
+            }));
+        }
+	}
+
+	async gameScore(userId) {
+		const top100 = await this.getTop100();
+
+        const currentRank = await zRevrank(['user_money', userId]);
+
+        let bottomList = await this.getRangeUsers(currentRank - 3, currentRank + 2);
+
+        // ilk 100 içerisinde olan elemanlar çıkarılıyor
+        for (let i = 5; i >= 0; i--) {
+            if (!bottomList[i])
+                continue;
+
+            if (top100.find(x => x.id === bottomList[i].id) !== undefined) {
+                bottomList = bottomList.slice(i+1);
+                break;
+            }
+        }
+
+        const result = [
+            ...top100,
+            ...bottomList,
+        ];
+
+        return result;
+	}
+
+	async endGame(userId, money) {
+        return await zIncrby(['user_money', money, userId]);
+	}
+}
+
+module.exports = new ScoreService();
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const redis = require('redis');
 const redisAdapter = require('@socket.io/redis-adapter');
 const jwtDecode = require('jwt-decode');
 
+const ScoreService = require('./ScoreService');
+
 const PORT = process.env.PORT || 8080;
 
 const REDIS_HOST = process.env.REDIS_HOST || 'localhost';
@@ -25,50 +27,7 @@ const pubClient = redis.createClient({
 const subClient = pubClient.duplicate();
 const redisClient = pubClient.duplicate();
 
-const setValue = promisify(redisClient.set).bind(redisClient);
-const getValue = promisify(redisClient.get).bind(redisClient);
-const mGet = promisify(redisClient.mget).bind(redisClient);
-const zAdd = promisify(redisClient.zadd).bind(redisClient);
-const zIncrby = promisify(redisClient.zincrby).bind(redisClient);
-const zRevrank = promisify(redisClient.zrevrank).bind(redisClient);
-const zCard = promisify(redisClient.zcard).bind(redisClient);
-const zRevrange = promisify(redisClient.zrevrange).bind(redisClient);
-const ZMscore = promisify(redisClient.zmscore).bind(redisClient);
-
-const getRangeUsers = async (min, max) => {
-    if (min === max)
-        return [];
-
-    const userScores = await zRevrange(['user_money', min, max, 'WITHSCORES']);
-
-    if (userScores.length < 1)
-        return [];
-
-    const userIds = [];
-    const moneys = [];
-    userScores.forEach((x, i) => {
-        if (i % 2 === 0)
-            userIds.push(x);
-        else
-            moneys.push(x);
-    });
-
-    const userTodayScores = await ZMscore(['user_today_money', ...userIds]);
-
-    const users = (await mGet(userIds.map(x => `user_${x}`))).map(JSON.parse);
-    let index = min;
-    users.forEach((x, i) => {
-        x.money = moneys[i];
-        x.todayRank = userTodayScores[i];
-        x.rank = index++;
-    });
-
-    return users;
-};
-
-const getTop100 = async () => {
-    return await getRangeUsers(0, 99);
-};
+ScoreService.redisInit(redisClient);
 
 io.adapter(redisAdapter(pubClient, subClient));
 
@@ -83,18 +42,7 @@ io.on('connection', async (socket) => {
     }
 
     try {
-        const redisUser = await zRevrank(['user_money', user.id]);
-        if (redisUser === null) {
-            await zAdd(['user_money', 0, user.id]);
-            const rank = await zRevrank(['user_money', user.id]);
-            await zAdd(['user_today_money', rank, user.id])
-
-            await setValue(`user_${user.id}`, JSON.stringify({
-                id: user.id,
-                name: user.name,
-                country: user.country,
-            }));
-        }
+        await ScoreService.checkUserAndCreate(user);
     }
     catch (e) {
         console.log('Veritabanı işleminde hata oluştu', e);
@@ -102,30 +50,9 @@ io.on('connection', async (socket) => {
     }
 
     const gameScore = async () => {
-        const top100 = await getTop100();
-
-        const currentRank = await zRevrank(['user_money', user.id]);
-
-        let bottomList = await getRangeUsers(currentRank - 3, currentRank + 2);
-
-        // ilk 100 içerisinde olan elemanlar çıkarılıyor
-        for (let i = 5; i >= 0; i--) {
-            if (!bottomList[i])
-                continue;
-
-            if (top100.find(x => x.id === bottomList[i].id) !== undefined) {
-                bottomList = bottomList.slice(i+1);
-                break;
-            }
-        }
-
-        const result = [
-            ...top100,
-            ...bottomList,
-        ];
+        const result = await ScoreService.gameScore(user.id);
 
         // TODO bir öncekinin aynısı ise dönülmeyecek
-
         socket.emit('score', result);
 
         return result;
@@ -137,7 +64,7 @@ io.on('connection', async (socket) => {
     // kişinin oyunu oynadıktan para kazandıran method
     socket.on('end-game', async (money = 1) => {
         try {
-            await zIncrby(['user_money', money, user.id]);
+            await ScoreService.endGame(user.id, money);
 
             // kişiye hemen dön
             await gameScore();
